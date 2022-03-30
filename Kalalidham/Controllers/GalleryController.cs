c73f7792687b15975f7cc4b27603092575bb97d8
Jay shah <shah.jay60@yahoo.com>
2022-03-30 20:29:10 +0530

    Commit gallery page

---
+++ Kalalidham/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using Kalalidham.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kalalidham.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        KalalidhamEntities usersEntities = new KalalidhamEntities();
+
+        public ActionResult Index()
+        {
+            var gallery = usersEntities.tblMultiImages.ToList();
+            return View();
+        }
+    }
+}
